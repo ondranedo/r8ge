04af49dfec802c3d955542fa1c4e3bd76d6b5d46
ondranedo <nedojedly.o@gmail.com>
2023-09-23 20:49:03 +0200

    core definitions

--- r8ge-core/src/Core.h
+++ r8ge-core/src/Core.h
@@ -1,10 +1,18 @@
 #ifndef R8GE_CORE_H
 #define R8GE_CORE_H
 
-#define R8GE_API
+#ifdef linux
+    #define R8GE_LINUX
+#elif _WIN32
+    #define R8GE_WINDOWS
+#else//!_WIN32
+    #error "Unknown/Unsupported platform"
+#endif//!linux
 
-namespace r8ge {
-    R8GE_API void test_linkage();
-}
+#if defined(R8GE_WINDOWS) && defined(R8GE_SHARED)
+    #define R8GE_API __declspec((__dllexport))
+#else
+    #define R8GE_API
+#endif//!R8GE_WINDOWS
 
 #endif//!R8GE_CORE_H
