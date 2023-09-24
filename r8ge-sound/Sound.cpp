9e9d93f1396718ab574d7d672b79a83e7e59ea4f
1nome <stashfoos@gmail.com>
2023-09-24 14:10:16 +0200

    New folders for files related to sound

---
+++ r8ge-sound/Sound.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Wiszc on 24.09.2023.
+//
+
+#include "Sound.hpp"
+#include <iostream>
+
+int r8ge::soundtestfunc() {
+    std::cout << " soudnTest succsesfzkt" << std::endl;
+    return 80085;
+}
---
+++ r8ge-sound/Sound.hpp
@@ -0,0 +1,12 @@
+//
+// Created by Wiszc on 24.09.2023.
+//
+
+#ifndef R8GE_SOUND_HPP
+#define R8GE_SOUND_HPP
+
+namespace r8ge {
+    int soundtestfunc();
+};
+
+#endif //R8GE_SOUND_HPP
