85d5dd7c91146945969c44460e453b9d8b27a85b
ondranedo <nedojedly.o@gmail.com>
2023-09-25 16:01:11 +0200

    Logger skeleton

--- r8ge-core/EntryPoint.cpp
+++ r8ge-core/EntryPoint.cpp
@@ -3,11 +3,21 @@
 #include <iostream>
 #include <ranges>
 
+#include "Logger.h"
+
 namespace r8ge {
     int main(const EntryPointData& entry_data)
     {
         for(auto const&[i, sv] : entry_data.argv | std::ranges::views::enumerate)
             std::cout << i << " : " <<  sv << std::endl;
+        //  R8GE_LOG("");   ->  TRACE
+        //  R8GE_LOG_TRACE("");
+        //      R8GE_LOG_DEBUG("");
+        //  R8GE_LOG_ERROR("");
+        //  R8GE_LOG_WARNI("");
+        //  R8GE_LOG_FATAL("");
+
+
 
         return 0;
     }
---
+++ r8ge-core/Logger.h
@@ -0,0 +1,12 @@
+#ifndef R8GE_LOGGER_H
+#define R8GE_LOGGER_H
+
+namespace r8ge {
+
+    class Logger {
+
+    };
+
+}
+
+#endif//!R8GE_LOGGER_H
