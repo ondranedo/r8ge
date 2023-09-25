c810d34efdb64fdf707373e49bd7aceaa8340cfe
ondranedo <nedojedly.o@gmail.com>
2023-09-25 20:54:01 +0200

    basic logger structure

--- r8ge-core/Logger.cpp
+++ r8ge-core/Logger.cpp
@@ -1,7 +1,51 @@
 
 #include "Logger.h"
 
+#include <iostream>
+
 namespace r8ge {
+    std::mutex mainLogger_mutex;
+    Logger* mainLogger;
 
+    Logger::Logger() : m_queue{}{}
+    Logger::~Logger() {
+        emptyLogQueue();
+    }
 
+    void Logger::log(Priority p, const std::string& raw) {
+        mainLogger_mutex.lock();
+        m_queue.emplace(raw, p);
+        mainLogger_mutex.unlock();
+
+        if(p == Priority::FATAL)
+            emptyLogQueue();
+    }
+
+    void Logger::emptyLogQueue() {
+        mainLogger_mutex.lock();
+        while(!m_queue.empty()) {
+            Log& l = m_queue.front();
+            m_queue.pop();
+
+            // TODO: Platform output
+            std::cout << format(l) << std::endl;
+        }
+        mainLogger_mutex.unlock();
+    }
+
+    std::string Logger::format(const Log& log) {
+        std::string str;
+        str+= "[";
+        switch (log.priority) {
+            case Priority::FATAL: str+= "fatal"; break;
+            case Priority::DEBUG: str+= "debug"; break;
+            case Priority::ERROR: str+= "error"; break;
+            case Priority::TRACE: str+= "trace"; break;
+            case Priority::WARNI: str+= "warni"; break;
+        }
+        str+= "] - " + log.raw_data;
+        return str;
+    }
+
+    Logger::Log::Log(const std::string &raw, Logger::Priority p) : raw_data(raw), priority(p) {}
 }
--- r8ge-core/Logger.h
+++ r8ge-core/Logger.h
@@ -1,12 +1,57 @@
 #ifndef R8GE_LOGGER_H
 #define R8GE_LOGGER_H
 
+#include <string>
+#include <chrono>
+#include <queue>
+#include <mutex>
+
+#include "utility/StringFormat.h"
+
 namespace r8ge {
+    class Logger;
+    extern Logger* mainLogger;
 
     class Logger {
+    public:
+        Logger(); // TODO: specify log file (default STDOUT), and format
+        ~Logger();
 
+        enum class Priority {
+            TRACE, DEBUG, ERROR, WARNI, FATAL
+        };
+
+        struct Log {
+            //std::chrono::time_point<std::chrono::high_resolution_clock> time_point; // TODO: Add platform time
+            std::string raw_data;
+            Priority priority;
+            Log(const std::string& raw, Priority p);
+        };
+
+        void log(Priority p, const std::string& str);
+        void emptyLogQueue();
+
+    private:
+        std::string format(const Log& log);
+
+        std::queue<Log> m_queue;
     };
 
+    template<typename ... Args>
+    void mainLog(Logger::Priority p, const std::string& parser, Args... args) {
+        if(mainLogger)
+            mainLogger->log(p, utility::StringFormat(parser, {args...}).to_string());
+
+        // TODO: Some error output or smth
+    }
 }
+
+#define R8GE_LOG_TRACE(...) r8ge::mainLog(r8ge::Logger::Priority::TRACE, __VA_ARGS__);
+#define R8GE_LOG_DEBUG(...) r8ge::mainLog(r8ge::Logger::Priority::DEBUG, __VA_ARGS__);
+#define R8GE_LOG_WARNI(...) r8ge::mainLog(r8ge::Logger::Priority::WARNI, __VA_ARGS__);
+#define R8GE_LOG_ERROR(...) r8ge::mainLog(r8ge::Logger::Priority::ERROR, __VA_ARGS__);
+#define R8GE_LOG_FATAL(...) r8ge::mainLog(r8ge::Logger::Priority::FATAL, __VA_ARGS__);
+
+//#define R8GE_LOG(...) R8GE_LOG_TRACE(__VA_ARGS__)z
 
 #endif//!R8GE_LOGGER_H
