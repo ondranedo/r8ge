d14f041e262d4d8c846629417fd7fe36a676914d
ondranedo <nedojedly.o@gmail.com>
2023-09-25 20:54:32 +0200

    entry point logger addition

--- r8ge-core/EntryPoint.cpp
+++ r8ge-core/EntryPoint.cpp
@@ -8,17 +8,9 @@
 namespace r8ge {
     int main(const EntryPointData& entry_data)
     {
-        for(auto const&[i, sv] : entry_data.argv | std::ranges::views::enumerate)
-            std::cout << i << " : " <<  sv << std::endl;
-        //  R8GE_LOG("");   ->  TRACE
-        //  R8GE_LOG_TRACE("");
-        //      R8GE_LOG_DEBUG("");
-        //  R8GE_LOG_ERROR("");
-        //  R8GE_LOG_WARNI("");
-        //  R8GE_LOG_FATAL("");
+        r8ge::mainLogger = new Logger;
 
-
-
+        delete r8ge::mainLogger;
         return 0;
     }
 }
