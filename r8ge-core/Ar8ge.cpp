976c01f2ef406ac52772a8415999def62a297d14
ondranedo <nedojedly.o@gmail.com>
2023-09-30 19:22:27 +0200

    EventQueue callback into Ar8ge

--- r8ge-core/Ar8ge.cpp
+++ r8ge-core/Ar8ge.cpp
@@ -3,19 +3,31 @@
 #include "Logger.h"
 
 namespace r8ge {
+    EventQueue::CallbackFn ar8geEventQueue = nullptr;
+
     Ar8ge::Ar8ge(): m_running(true){
         R8GE_LOG("Engine application created");
-    };
+
+        ar8geEventQueue = m_queue.getCallbackFn();
+        R8GE_LOG("Ar8ge event queue set - events may be received");
+    }
 
     Ar8ge::~Ar8ge() {
         R8GE_LOG("Engine application destroyed");
+
+
+        ar8geEventQueue = nullptr;
+        R8GE_LOG("Ar8ge event queue set to nullptr - events all event are lost");
     }
 
     void Ar8ge::run() {
         R8GE_LOG("Engine application starting main loop");
 
-        while(m_running) {
+        /*while(m_running) {
+
+            m_queue.emptyQueue();
             mainLogger->emptyLogQueue();
-        }
+        }*/
+
     }
 }
--- r8ge-core/Ar8ge.h
+++ r8ge-core/Ar8ge.h
@@ -2,19 +2,22 @@
 #define R8GE_AR8GE_H
 
 #include "Core.h"
+#include "events/EventQueue.h"
 
 namespace r8ge {
+    extern EventQueue::CallbackFn ar8geEventQueue;
+
     class Ar8ge {
     public:
         Ar8ge();
         ~Ar8ge();
 
-        void init();
         void run();
-        void release();
+
 
     private:
         bool m_running;
+        EventQueue m_queue;
     };
 }
 
