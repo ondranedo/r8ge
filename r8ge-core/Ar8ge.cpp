cf3587ad425c3ac17df3502b4407794f39ef921e
ondranedo <nedojedly.o@gmail.com>
2023-10-01 16:16:42 +0200

    - Window Events - Ar8ge log reordering

--- r8ge-core/Ar8ge.cpp
+++ r8ge-core/Ar8ge.cpp
@@ -6,18 +6,18 @@ namespace r8ge {
     EventQueue::CallbackFn ar8geEventQueue = nullptr;
 
     Ar8ge::Ar8ge(): m_running(true){
-        R8GE_LOG("Engine application created");
-
         ar8geEventQueue = m_queue.getCallbackFn();
         R8GE_LOG("Ar8ge event queue set - events may be received");
+
+        R8GE_LOG("Engine application created");
     }
 
     Ar8ge::~Ar8ge() {
-        R8GE_LOG("Engine application destroyed");
-
-
         ar8geEventQueue = nullptr;
-        R8GE_LOG("Ar8ge event queue set to nullptr - events all event are lost");
+        R8GE_LOG("Ar8ge event queue set to nullptr");
+
+
+        R8GE_LOG("Engine application destroyed");
     }
 
     void Ar8ge::run() {
