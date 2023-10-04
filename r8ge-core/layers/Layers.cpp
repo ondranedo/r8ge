7119f899decf4e81823a9c1f8fef056564df2a83
ondranedo <nedojedly.o@gmail.com>
2023-10-04 19:37:43 +0200

    Layer skeleton

--- r8ge-core/layers/Layers.cpp
+++ r8ge-core/layers/Layers.cpp
@@ -1,1 +1,12 @@
 #include "Layers.h"
+
+namespace r8ge {
+    Layer::Layer(const std::string &name) : m_name() {}
+
+    Layer::~Layer() = default;
+
+    std::string Layer::getName() const {
+        return m_name;
+    }
+
+}
--- r8ge-core/layers/Layers.h
+++ r8ge-core/layers/Layers.h
@@ -1,4 +1,26 @@
 #ifndef R8GE_LAYERS_H
 #define R8GE_LAYERS_H
 
+#include "../Core.h"
+#include "../events/Event.h"
+
+#include <memory>
+
+namespace r8ge {
+    R8GE_API class Layer {
+    public:
+        explicit Layer(const std::string& name);
+        virtual ~Layer();
+
+        virtual void update() = 0;
+        virtual void event(const std::shared_ptr<Event>& event) = 0;
+        virtual void render() = 0;
+
+        [[nodiscard]] std::string getName() const;
+
+    private:
+        std::string m_name;
+    };
+}
+
 #endif//!R8GE_LAYERS_H
