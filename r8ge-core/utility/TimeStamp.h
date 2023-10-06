60e9892d7916a73f7d54748f7243d927ad5b2078
ondranedo <nedojedly.o@gmail.com>
2023-10-06 16:00:36 +0200

    Comments for TimeStamp

--- r8ge-core/utility/TimeStamp.h
+++ r8ge-core/utility/TimeStamp.h
@@ -12,10 +12,36 @@ namespace r8ge {
         };
         TimeStamp();
 
-        TimeStamp& operator-(const TimeStamp&);
+        // Converts localtime (time since epoch ISO 8601) to `TimeStamp::_point`,
+        // if we use this for differing between times, an error is thrown.
+        //    TimeStamp t1; // Time is: 2023-10-06 15:15:16 ....
+        //    // ...
+        //
+        //    t1.sinceEpoch(TimeStamp::SECOND); -> 16
+        //    t1.sinceEpoch(TimeStamp::HOUR); -> 15
+        //    t1.sinceEpoch(TimeStamp::YEAR); -> 2023
+        //    t1.sinceEpoch(TimeStamp::DAY); -> 6
         [[nodiscard]] std::size_t sinceEpoch(TimeStamp::_point timePoint) const;
+
+        // Counts how many `TimeStamp::_point` have passed since POSIX time
+        // ,not counting leap seconds (in ISO 8601: 1970-01-01T00:00:00Z).
+        // If we use `TimeStamp` in calculation f.e:
+        //    TimeStamp t1;
+        //    // ...
+        //    TimeStamp t2;
+        //    (t2-t1).count(TimeStamp::SECOND);
+        // It outputs how many seconds have passed since `t1` creation to `t2` creation
         [[nodiscard]] std::size_t count(TimeStamp::_point timePoint) const;
+
+        // Uses https://strftime.org/ format, with addition to custom time formats for formating
+        // milliseconds - %^{ms}
+        // microseconds - %^{us}
+        // nanoseconds  - %^{ns}
         [[nodiscard]] std::string to_string(const std::string& format = "%Y:%m:%d %H:%M:%S") const;
+
+    public:
+        TimeStamp& operator-(const TimeStamp&);
+
     private:
         // Used for measuring time
         std::size_t m_nanoseconds; // Since epoch
