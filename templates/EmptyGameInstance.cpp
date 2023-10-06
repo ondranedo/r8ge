45361b6d7f67e779c4609c7ddab99f1425aaabd6
ondranedo <nedojedly.o@gmail.com>
2023-10-06 18:00:45 +0200

    Basic tempalte

---
+++ templates/EmptyGameInstance.cpp
@@ -0,0 +1,27 @@
+// Includes may cause problems, make sure to include them properly
+// one include file is not yet written
+
+#include "../r8ge-core/Core.h"
+#include "../r8ge-core/instance/GameInstance.h"
+#include "../r8ge-core/Logger.h"
+
+class TempalteGame : public r8ge::GameInstance {
+public:
+    TempalteGame() : r8ge::GameInstance("TempalteGame") {}
+    ~TempalteGame() override = default;
+
+    void onInit()  override {
+        R8GE_LOG("`{}` game initialization", getName());
+    }
+
+    void onUpdate() override {
+
+    }
+
+    void onExit()  override {
+        R8GE_LOG("`{}` game exiting", getName());
+    }
+};
+
+R8GE_ADD_GAMEINSTANCE(TempalteGame);
+
