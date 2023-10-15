963741bc4e0ab0c0ed054a8754741d8ac2977ab2
1nome <stashfoos@gmail.com>
2023-10-15 17:00:38 +0200

    loadMIDI progress 2

--- r8ge-sound/Sequencer.cpp
+++ r8ge-sound/Sequencer.cpp
@@ -28,13 +28,13 @@ void r8ge::MIDISequencer::loadMIDI(const std::string& filename) {
     cStringBuff[4] = '\0';
     std::string cmp;
 
-    long chunklen;
+    unsigned long chunklen = 0;
     short format;
     short nTracks;
     short tickdiv;
     long delay;
-    char buff;
-    long long ticks;
+    unsigned char buff;
+    unsigned long ticks;
 
     std::ifstream file(filename, std::ios::binary);
     if(!file.is_open()){
@@ -49,15 +49,20 @@ void r8ge::MIDISequencer::loadMIDI(const std::string& filename) {
         goto Exit;
     }
 // the following chunklen checks are unnecessary but might serve for debugging purposes
+    // byteswap because midi files are big endian ffs
     file.read(reinterpret_cast<char*>(&chunklen), 4);
+    chunklen = std::byteswap(chunklen);
     if(chunklen >= 2){
         file.read(reinterpret_cast<char*>(&format), 2);
+        format = std::byteswap(format);
     }
     if(chunklen >= 4){
         file.read(reinterpret_cast<char*>(&nTracks), 2);
+        nTracks = std::byteswap(nTracks);
     }
     if(chunklen >= 6){
         file.read(reinterpret_cast<char*>(&tickdiv), 2);
+        tickdiv = std::byteswap(tickdiv);
     }
 
     if(format == 0 && nTracks > 1){
@@ -70,8 +75,25 @@ void r8ge::MIDISequencer::loadMIDI(const std::string& filename) {
     }
 
     for(int i = 0; i < nTracks; i++){
-        bool runningStatus = false;
+        enum State{
+            READING_TIME,
+            READING_EVENT,
+            NOTE_OFF = 0x80,
+            NOTE_ON = 0x90,
+            POLYPHONIC_PRESSURE = 0xA0,
+            CONTROLLER = 0xB0,
+            PROGRAM_CHANGE = 0xC0,
+            CHANNEL_PRESSURE = 0xD0,
+            PITCH_BEND = 0xE0,
+            SYSEX_MSG = 0xF0,
+            ESCAPE_SEQUENCE = 0xF7,
+            META_EVENT = 0xFF
+        } state = READING_TIME;
+        State previous = NOTE_OFF;
         ticks = 0;
+        delay = 0;
+
+
         file.read(cStringBuff, 4);
         cmp = cStringBuff;
         if(cmp != "MTrk"){
@@ -80,21 +102,54 @@ void r8ge::MIDISequencer::loadMIDI(const std::string& filename) {
         }
 
         file.read(reinterpret_cast<char*>(&chunklen), 4);
-        for(long j = 0; j < chunklen;){
-            //get the delay between events
-            delay = 0;
-            int b = 0;
-            do{
-                delay = delay << 7;
-                file.read(&buff, 1);
-                delay += buff & ~(1 << 7);
-                b++;
-            }while(buff & (1 << 7) && b != 4);
-            j += b;
-            ticks += delay;
-
-            // process the event;
-            file.read(&buff, 1);
+        chunklen = std::byteswap(chunklen);
+        for(long j = 0; j < chunklen; j++){
+            file.read(reinterpret_cast<char*>(&buff), 1);
+            switch (state) {
+                case READING_TIME:
+                    delay += buff & 0x7F;
+                    if(buff & 0x80){
+                        delay = delay << 7;
+                    }
+                    else{
+                        state = READING_EVENT;
+                        ticks += delay;
+                        delay = 0;
+                    }
+                    break;
+                case READING_EVENT:
+                    if(buff < 0x80){
+                        state = previous;
+                    }
+                    else if(buff < 0xF0){
+                        state = static_cast<State>(buff & 0xF0);
+                        previous = state;
+                    }
+                    else{
+                        state = static_cast<State>(buff);
+                    }
+                    break;
+                case NOTE_OFF:
+                    break;
+                case NOTE_ON:
+                    break;
+                case POLYPHONIC_PRESSURE:
+                    break;
+                case CONTROLLER:
+                    break;
+                case PROGRAM_CHANGE:
+                    break;
+                case CHANNEL_PRESSURE:
+                    break;
+                case PITCH_BEND:
+                    break;
+                case SYSEX_MSG:
+                    break;
+                case ESCAPE_SEQUENCE:
+                    break;
+                case META_EVENT:
+                    break;
+            }
         }
     }
 
