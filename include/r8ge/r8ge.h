aadbb7a075d83e36079f328774d0ffa852705583
karnatour <vojtasokala@gmail.com>
2024-01-25 22:07:53 +0100

    Remove gltf file

--- include/r8ge/r8ge.h
+++ include/r8ge/r8ge.h
@@ -19,7 +19,6 @@
 #include "../r8ge-core/fileio/fileReaders/Binary.h"
 #include "../r8ge-core/fileio/fileReaders/Text.h"
 #include "../r8ge-core/fileio/fileReaders/Glsl.h"
-#include "../r8ge-core/fileio/fileReaders/Gltf.h"
 #include "../r8ge-core/fileio/fileReaders/Wave.hpp"
 #include "../r8ge-core/Ar8ge.h"
 #include "../r8ge-core/events/Strokes.h"
