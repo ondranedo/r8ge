ac63754a234bb2eaf04f5b20e8f3649c2495b6f4
karnatour <vojtasokala@gmail.com>
2024-02-09 15:42:13 +0100

    Entity

--- r8ge-video/renderer/Entity.cpp
+++ r8ge-video/renderer/Entity.cpp
@@ -3,31 +3,94 @@
 //
 
 #include "Entity.h"
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <random>
+#include "../Video.h"
 
 namespace r8ge {
     namespace video {
 
         unsigned long Entity::m_nextID = 0;
 
-        unsigned long Entity::getEntityID() {
+        Entity::Entity(Scene &scene) : m_id(m_nextID++),m_scene(scene) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<float> dis(0.5f, 1.0f);
+            std::uniform_real_distribution<float> dis1(-1.0f, 1.0f);
+            std::uniform_real_distribution<float> dis3(-10.0f, 10.0f);
+            Video::getRenderingService()->compileProgram(m_shader);
+            m_transformation.model = glm::mat4(1.0f);
+            m_transformation.projection = glm::perspective(glm::radians(45.0f),
+                                                           static_cast<float>(Video::getWindowingService()->getWidth()) /
+                                                           static_cast<float>(Video::getWindowingService()->getHeight()), 0.1f, 100.0f);
+
+            m_transformation.model = glm::translate(m_transformation.model,glm::vec3(dis3(gen),dis1(gen),dis1(gen)));
+            m_transformation.model = glm::scale(m_transformation.model,glm::vec3(dis(gen),dis(gen),dis(gen)));
+
+        }
+
+        unsigned long Entity::getEntityID() const {
             return m_id;
         }
 
+        void Entity::changeTexture(const Texture2D &texture) {
+            m_texture.setData(texture);
+            m_hasTexture = true;
+        }
+
+        void Entity::changeMaterial(const Material &material) {
+            m_material = material;
+            m_hasMaterial = true;
+        }
+
+        void Entity::changeTransformation(const Transformation &transformation) {
+            m_transformation = transformation;
+        }
+
+        bool Entity::getTextureState() const {
+            return m_hasTexture;
+        }
+
+        bool Entity::getMaterialState() const {
+            return m_hasMaterial;
+        }
+
+        void Entity::setUpdateFunction(Entity::UpdateFunction function) {
+            m_updateFunction = function;
+        }
+
         void EntityModel::render() {
-            //m_model.render();
+            if (m_updateFunction != nullptr) {
+                m_updateFunction();
+            }
+            m_transformation.view = m_scene.getCamera().getViewMatrix();
+            m_model.render(m_shader,m_transformation);
         }
 
-        void EntityModel::update(float deltaTime) {
 
+        void EntityCube::render() {
+            if (m_updateFunction != nullptr) {
+                m_updateFunction();
+            }
+            m_transformation.view = m_scene.getCamera().getViewMatrix();
+            m_cubeMesh.render(m_shader,m_transformation);
         }
 
-        void EntityModel::attachModel(const std::string& path) {
-            Model temp(path);
-            m_model = temp;
+        void EntitySphere::render() {
+            if (m_updateFunction != nullptr) {
+                m_updateFunction();
+            }
+            m_transformation.view = m_scene.getCamera().getViewMatrix();
+            m_sphereMesh.render(m_shader,m_transformation);
         }
 
-        Entity *EntityModel::clone() const {
-            return new EntityModel(*this);
+        void EntityCylinder::render() {
+            if (m_updateFunction != nullptr) {
+                m_updateFunction();
+            }
+            m_transformation.view = m_scene.getCamera().getViewMatrix();
+            m_cylinderMesh.render(m_shader,m_transformation);
         }
 
     } // r8ge
--- r8ge-video/renderer/Entity.h
+++ r8ge-video/renderer/Entity.h
@@ -6,74 +6,95 @@
 #define R8GE_ENTITY_H
 
 #include "Model.h"
+#include "Mesh.h"
 #include <utility>
 
 namespace r8ge {
     namespace video {
 
+        class Scene;
+
         class Entity {
         public:
-            Entity() : m_id(m_nextID++) {};
+            typedef void (*UpdateFunction)();
 
-            unsigned long getEntityID();
+            explicit Entity(Scene &scene);
+
+            [[nodiscard]] unsigned long getEntityID() const;
 
             virtual void render() = 0;
 
-            virtual void update(float deltaTime) = 0;
+            void setUpdateFunction(UpdateFunction function);
 
-            [[nodiscard]] virtual Entity* clone() const = 0;
+            //[[nodiscard]] virtual Entity *clone() const = 0;
 
-            void attachTexture();
+            void changeTexture(const Texture2D &texture);
 
-            bool getMaterialState();
+            void changeMaterial(const Material &material);
 
-            bool getTextureState();
+            void changeTransformation(const Transformation &transformation);
+
+            [[nodiscard]] bool getMaterialState() const;
+
+            [[nodiscard]] bool getTextureState() const;
+
+            virtual ~Entity() = default;
 
         protected:
             static unsigned long m_nextID;
             unsigned long m_id;
             bool m_hasMaterial = false;
             bool m_hasTexture = false;
-            //Program m_shader;
+            UpdateFunction m_updateFunction{};
+            Program m_shader = Program("Engine/Shaders/EntityDefault.glsl");
+            GLTexture m_texture;
+            Material m_material{};
+            Transformation m_transformation;
+            Scene &m_scene;
+
         };
 
         class EntityModel : public Entity {
         public:
+            explicit EntityModel(Scene &scene, Model model) : Entity(scene), m_model(std::move(model)) {};
+
             void render() override;
 
-            void update(float deltaTime) override;
-
-            Entity* clone() const override;
-
-            void attachModel(const std::string& path);
         private:
             Model m_model;
         };
 
+
         class EntityCube : public Entity {
         public:
+            explicit EntityCube(Scene &scene, Mesh cubeMesh) : Entity(scene), m_cubeMesh(std::move(cubeMesh)) {};
+
             void render() override;
-            void update(float deltaTime) override;
 
         private:
+            Mesh m_cubeMesh;
         };
 
         class EntitySphere : public Entity {
         public:
+            explicit EntitySphere(Scene &scene, Mesh sphereMesh) : Entity(scene),
+                                                                   m_sphereMesh(std::move(sphereMesh)) {};
+
             void render() override;
 
-            void update(float deltaTime) override;
-
         private:
+            Mesh m_sphereMesh;
         };
 
         class EntityCylinder : public Entity {
         public:
+            explicit EntityCylinder(Scene &scene, Mesh cylinderMesh) : Entity(scene),
+                                                                       m_cylinderMesh(std::move(cylinderMesh)) {};
+
             void render() override;
 
-            void update(float deltaTime) override;
-
         private:
+            Mesh m_cylinderMesh;
         };
 
     } // r8ge
